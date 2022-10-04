bd8833cb4eb557e30e99750f506772540750e6de
hannsch98 <101269571+hannsch98@users.noreply.github.com>
2022-10-04 19:16:38 +0200

    Object and Array destructuring

--- first-app.js
+++ first-app.js
@@ -6,24 +6,38 @@ const person = {
 	},
 };
 
-//person.greet();
+//object destructuring:
+//instead of personData.name as property => {name}
+const printName = ({ name }) => {
+	console.log(name);
+};
+
+printName(person);
+
+const { name, age } = person;
+console.log(name, age);
 
 const hobbies = ["Running", "Dying", "Fighting"];
-/* for (let hobby of hobbies) {
-	console.log(hobby);
-} */
 
-//console.log(hobbies.map((hobby) => "Hobby: " + hobby));
-//array.map edits the array without changing it (for display reasons for example)
+//array destructuring:
+const [hobby1, hobby2] = hobbies;
+console.log(hobby1, hobby2);
 
-hobbies.push("Being gay");
+// /* for (let hobby of hobbies) {
+// 	console.log(hobby);
+// } */
 
-const copiedArray = [...hobbies, "sleeping"];
-//... is like ++ or --
-//spread operator
+// //console.log(hobbies.map((hobby) => "Hobby: " + hobby));
+// //array.map edits the array without changing it (for display reasons for example)
 
-const toArray = (...args) => args;
-//... in this case called rest operator
-//
+// hobbies.push("Being gay");
 
-console.log(toArray(1, 2, 3, 4));
+// const copiedArray = [...hobbies, "sleeping"];
+// //... is like ++ or --
+// //spread operator
+
+// const toArray = (...args) => args;
+// //... in this case called rest operator
+// //
+
+// console.log(toArray(1, 2, 3, 4));
