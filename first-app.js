442471cf72a23aa4395d0fbfe54286cc83f079f7
hannsch98 <101269571+hannsch98@users.noreply.github.com>
2022-10-04 19:10:40 +0200

    Spread and Rest Operator

--- first-app.js
+++ first-app.js
@@ -6,13 +6,24 @@ const person = {
 	},
 };
 
-person.greet();
+//person.greet();
 
 const hobbies = ["Running", "Dying", "Fighting"];
 /* for (let hobby of hobbies) {
 	console.log(hobby);
 } */
-console.log(hobbies.map((hobby) => "Hobby: " + hobby));
+
+//console.log(hobbies.map((hobby) => "Hobby: " + hobby));
 //array.map edits the array without changing it (for display reasons for example)
+
 hobbies.push("Being gay");
-console.log(hobbies);
+
+const copiedArray = [...hobbies, "sleeping"];
+//... is like ++ or --
+//spread operator
+
+const toArray = (...args) => args;
+//... in this case called rest operator
+//
+
+console.log(toArray(1, 2, 3, 4));
